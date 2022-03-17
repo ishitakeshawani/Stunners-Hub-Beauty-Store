437bd171d6c0374320bec5c7fefe96777860a233
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-17 21:09:06 +0530

    added top brands

---
+++ src/backend/db/topbrands.js
@@ -0,0 +1,9 @@
+import brand1 from "../../Components/Home/Images/brand1.jpg";
+import brand2 from "../../Components/Home/Images/brand2.jpg";
+import brand3 from "../../Components/Home/Images/brand3.jpg";
+import brand4 from "../../Components/Home/Images/brand4.jpg";
+
+
+const topbrandslist = [brand1, brand2, brand3, brand4];
+
+export { topbrandslist };
