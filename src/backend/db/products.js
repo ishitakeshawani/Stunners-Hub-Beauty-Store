ad04402f310c73441460410c7ba9f02d06d02d88
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-23 20:48:17 +0530

    added products

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -8,23 +8,158 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    name: "Eye Look Combo- Mascara + Eyeliner + Kajal",
+    price: "275",
+    categoryName: "eyes",
+    rate: 4,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/9/a/9a6872bNYNYKBC001512_1.jpg",
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    name: "Lakme Eyeconic Kajal - Regal Green",
+    price: "234",
+    categoryName: "eyes",
+    rate: 3,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/3/3/33ef0c5NYKAC00000132_0.jpg",
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    name: "Maybelline New York Fit Me Matte+Poreless Liquid Foundation With Clay - 115 Ivory",
+    price: "507",
+    categoryName: "face",
+    rate: 4,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/6/6/66e3c566902395646860_new_1.jpg",
   },
+  {
+    _id: uuid(),
+    name: "Lakme Absolute White Intense Wet & Dry Compact - 01 Classic Ivory",
+    price: "700",
+    categoryName: "face",
+    rate: 4,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/2/2/22680_s1-8901030366734m.jpg",
+  },
+  {
+    _id: uuid(),
+    name: "Nykaa Matte to Last! Transfer Proof Liquid Lipstick - IT Girl - 06",
+    price: "499",
+    categoryName: "lips",
+    rate: 4,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/8/9/8904245704209_1_3.jpg",
+  },
+  {
+    _id: uuid(),
+    name: "Kay Beauty Matte Action Lip Liner - Sensational",
+    price: "468",
+    categoryName: "lips",
+    rate: 4,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/0/3/0380cf5kay_mtalp_1.jpg",
+  },
+  {
+    _id: uuid(),
+    name: "Kay Beauty Nail Nourish Nail Enamel Polish - Hibiscus 28",
+    price: "180",
+    categoryName: "nails",
+    rate: 4,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/d/a/dad5c788904330901278_1.jpg",
+  },
+  {
+    _id: uuid(),
+    name: "Lakme Color Crush Nail Art - S8",
+    price: "132",
+    categoryName: "nails",
+    rate: 3,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/1/d/1d0bf218901030715938_1.jpg",
+  },
+  {
+    _id: uuid(),
+    name: "MCaffeine Complete Coffee Skin Care Combo",
+    price: "915",
+    categoryName: "cleaners",
+    rate: 4,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/f/a/fab9509nymcfcom00030_1.jpg",
+  },
+  {
+    _id: uuid(),
+    name: "Simple Kind To Skin Micellar Cleansing Water + Wipes",
+    price: "617",
+    categoryName: "cleaners",
+    rate: 3,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/6/8/68f3dc127280+27282_1.jpg",
+  },
+  {
+    _id: uuid(),
+    name: "NIVEA Body Lotion, Aloe Hydration, with Aloe Vera, for Smooth, Hydrated Skin",
+    price: "220",
+    categoryName: "bodycare",
+    rate: 4,
+    image:
+      "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/b/4/b4515ae8904256002479new.jpg",
+  },
+  {
+    _id: uuid(),
+    name: "Cetaphil Baby Mild Bar",
+    price: "180",
+    categoryName: "bodycare",
+    rate: 3,
+    image:"https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/3/5/35979108904027305815_1.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "L'Oreal Professionnel Absolut Repair Shampoo with Protein and Gold Quinoa, Serie Expert",
+    price: "695",
+    categoryName: "Shampoo",
+    rate: 2,
+    image:"https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/0/9/0938a71nyloprof00002_0.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Livon Hair Serum for Women & Men| All Hair Types |Smooth, Frizz free & Glossy Hair",
+    price: "60",
+    categoryName: "hairserum",
+    rate: 4,
+    image:"https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/4/9/4991c9c8901088062374_1.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Moi By Nykaa Matin Eau de Perfume",
+    price: "800",
+    categoryName: "perfumes",
+    rate: 4,
+    image:"https://images-static.nykaa.com/media/catalog/product/1/3/1359e368904245710668_1.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Guerlain Aqua Allegoria Flora Cherrysia Eau De Toilette",
+    price: "1500",
+    categoryName: "perfumes",
+    rate: 4,
+    image:"https://images-static.nykaa.com/media/catalog/product/g/0/g013652.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "NIVEA Women Deodorant, Fresh Flower, Long Lasting Freshness & 48h Protection",
+    price: "200",
+    categoryName: "deodorants",
+    rate: 3,
+    image:"https://images-static.nykaa.com/media/catalog/product/7/f/7f6ea964005900243249_1a.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Nykaa Wanderlust Deodorant Spray - Mediterranean Sea Salt",
+    price: "400",
+    categoryName: "deodorants",
+    rate: 1,
+    image:"https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/8/9/8904245713867_1_1_1.jpg"
+  },
+
 ];
