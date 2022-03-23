7ddc9fa149d5af84b34ef60dbcafe0ddce73fd73
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-23 20:42:23 +0530

    added products page

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Filter, ProductsList } from '../../Components';
+import "./products.css";
+
+
+export default function Products() {
+  return (
+    <div className="product-page">
+        <Filter />
+        <ProductsList />
+    </div>
+  )
+}
