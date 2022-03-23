291e2b5b5e4ec8b1ef0a55050870d0c94aa6c0f6
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-23 20:47:34 +0530

    added components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,5 @@
 export { Footer } from "./Footer/Footer";
 export { Navbar } from "./Navbar/Navbar";
+export { Homepage } from "./Home/Homepage";
+export { Filter } from "./Filter/Filter";
+export { ProductsList } from "./ProductsList/ProductsList";
