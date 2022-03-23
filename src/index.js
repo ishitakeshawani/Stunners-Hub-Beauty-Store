39b9665c06cd93b7887a76efb82d729bf3db4fc2
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-23 20:45:28 +0530

    added productprovider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ProductProvider } from "./contexts/ProductProvider";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ProductProvider>
+        <App />
+      </ProductProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
