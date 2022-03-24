cc13e5ac0feea8b2292734757e8b55746d6992ec
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-24 15:14:10 +0530

    added getFilteredData

--- src/contexts/addFilters.js
+++ src/contexts/addFilters.js
@@ -2,57 +2,86 @@ export const addFilters = (state, { type, payload }) => {
   switch (type) {
     case "CALL_API":
       return { ...state, productList: payload };
-    case "SORT":
+    case "SORT": {
       if (payload === "PRICE_HIGH_TO_LOW") {
+        console.log("1star", state);
         return { ...state, sortBy: "PRICE_HIGH_TO_LOW" };
       }
       if (payload === "PRICE_LOW_TO_HIGH") {
         return { ...state, sortBy: "PRICE_LOW_TO_HIGH" };
       }
+      return { ...state, sortBy: "" };
+    }
+    case "FILTER_BY_RATE": {
+      if (payload === "1STAR_AND_ABOVE") {
+        return {
+          ...state,
+          FilterData: {
+            ...state.FilterData,
+            filterByRate: state.productList.filter(
+              (product) => product.rate >= 1
+            ),
+          },
+        };
+      }
+      if (payload === "2STAR_AND_ABOVE") {
+        return {
+          ...state,
+          FilterData: {
+            ...state.FilterData,
+            filterByRate: state.productList.filter(
+              (product) => product.rate >= 2
+            ),
+          },
+        };
+      }
+      if (payload === "3STAR_AND_ABOVE") {
+        return {
+          ...state,
+          FilterData: {
+            ...state.FilterData,
+            filterByRate: state.productList.filter(
+              (product) => product.rate >= 3
+            ),
+          },
+        };
+      }
+      if (payload === "4STAR_AND_ABOVE") {
+        return {
+          ...state,
+          FilterData: {
+            ...state.FilterData,
+            filterByRate: state.productList.filter(
+              (product) => product.rate >= 4
+            ),
+          },
+        };
+      }
+    }
 
-      // case "SORT_BY_4STAR_AND_ABOVE":
-      //   return {
-      //     ...state,
-      //     productList: action.payload.filter((product) => product.rate >= 4),
-      //   };
-      // case "SORT_BY_3STAR_AND_ABOVE":
-      //   return {
-      //     ...state,
-      //     productList: action.payload.filter((product) => product.rate >= 3),
-      //   };
-      // case "SORT_BY_2STAR_AND_ABOVE":
-      //   return {
-      //     ...state,
-      //     productList: action.payload.filter((product) => product.rate >= 2),
-      //   };
-      // case "SORT_BY_1STAR_AND_ABOVE":
-      //   return {
-      //     ...state,
-      //     productList: action.payload.filter((product) => product.rate >= 1),
-      //   };
-      // case "Perfumes":
-      //   return {
-      //     ...state,
-      //     productList: state.productList.filter(
-      //       (product) => product.categoryName === "perfumes"
-      //     ),
-      //     // getFilteredProducts("perfumes", action.payload),
-      //     // [...new Set([
-      //     //   ...action.payload.filter(
-      //     //    (product) => product.categoryName == "perfumes"
-      //     //  ), ...state.productList])]
-      //   };
-      // case "Dedorants":
-      return {
-        // ...state,
-        // productList: state.productList.filter(
-        //   (product) => product !== action.payload
-        // ),
-        // [...new Set([
-        //   ...action.payload.filter(
-        //    (product) => product.categoryName == "deodorants"
-        //  ),...state.productList])]
-      };
+    //   // case "Perfumes":
+    //   //   return {
+    //   //     ...state,
+    //   //     productList: state.productList.filter(
+    //   //       (product) => product.categoryName === "perfumes"
+    //   //     ),
+    //   //     // getFilteredProducts("perfumes", action.payload),
+    //   //     // [...new Set([
+    //   //     //   ...action.payload.filter(
+    //   //     //    (product) => product.categoryName == "perfumes"
+    //   //     //  ), ...state.productList])]
+    //   //   };
+    //   // case "Dedorants":
+    //   return {
+    //     // ...state,
+    //     // productList: state.productList.filter(
+    //     //   (product) => product !== action.payload
+    //     // ),
+    //     // [...new Set([
+    //     //   ...action.payload.filter(
+    //     //    (product) => product.categoryName == "deodorants"
+    //     //  ),...state.productList])]
+    //   };
     default:
       return { ...state };
   }
