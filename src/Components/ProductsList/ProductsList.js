2898ac5bd11672033d588c1287214981f8952116
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-27 22:45:23 +0530

    added discount of product

--- src/Components/ProductsList/ProductsList.js
+++ src/Components/ProductsList/ProductsList.js
@@ -1,15 +1,29 @@
 import { React, useState, useEffect } from "react";
-import { useProduct } from "../../contexts/ProductProvider";
+import { useProduct } from "../../contexts/ProductProvider/ProductProvider";
+import { useCart } from "../../contexts/CartProvider/CartProvider";
 import "../Products/products.css";
-import { getSortedData, getFilteredData, filterByRate, getFilterByPrice } from "../Filter/Filter";
+import {
+  getSortedData,
+  getFilteredData,
+  filterByRate,
+  getFilterByPrice,
+} from "../Filter/Filter";
+import { addProductToCart } from "../../utils/cartUtils";
+import { Navigate } from "react-router-dom";
 
 export function ProductsList() {
   const { state } = useProduct();
+  const { cartState, dispatch } = useCart();
   const filterByPriceData = getFilterByPrice(state, state.productList);
   const filterData = getFilteredData(state, filterByPriceData);
   const filterByRateData = filterByRate(state, filterData);
   const sortedData = getSortedData(state, filterByRateData);
- 
+  function ProductInCart(productId) {
+    // console.log(cartState);
+    return cartState.cartProductList.some(
+      (product) => product._id === productId
+    );
+  }
 
   return (
     <div className="cards-list">
@@ -20,13 +34,24 @@ export function ProductsList() {
               <img className="product-img" src={val.image} alt="" />
               <div className="product-card-title">{val.name}</div>
               <div className="card-mrp">
-                <span className="gray-color">MRP:</span> ₹{val.price}
+                <span className="gray-color">MRP:</span> ₹{val.price}<span className="product-discount">{val.discount}% off</span>
               </div>
               <Rate rate={val.rate} />
             </div>
             <div className="product-card-footer pink-color">
               <i className="card-footer-icon fa-regular fa-heart"></i>
-              <button className="btn card-btn">Add to Cart</button>
+              <button
+                className="btn card-btn"
+                onClick={() => {
+                  ProductInCart(val._id)
+                    ? console.log("already in cart")
+                    : addProductToCart(val,dispatch,cartState);
+                    console.log( ProductInCart(val._id))
+                    console.log(cartState.cartProductList);
+                }}
+              >
+                {ProductInCart(val._id) ? "Go To Cart" : "Add to cart"}
+              </button>
             </div>
           </div>
         ))
