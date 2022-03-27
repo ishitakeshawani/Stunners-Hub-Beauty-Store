1dabfe8b884e185501976768bff568580f2c554d
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-27 22:34:46 +0530

    added route for cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Homepage, Navbar, Login, Signup } from "./Components";
+import { Homepage, Navbar, Login, Signup, Cart } from "./Components";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Products from "./Components/Products/Products";
@@ -13,6 +13,7 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
