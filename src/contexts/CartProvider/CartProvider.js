cd076abf7640128399a5b470baed7ea49ace5e43
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-27 22:19:52 +0530

    added cart provider

---
+++ src/contexts/CartProvider/CartProvider.js
@@ -0,0 +1,39 @@
+import { React, useContext, createContext, useState, useReducer } from "react";
+import { cartReducer } from "./cartReducer";
+import { TotalCartPrice, TotalProductDiscount } from "../../utils/cartUtils";
+const cartContext = createContext();
+
+function CartProvider({ children }) {
+  const [name, setName] = useState();
+  const initialState = {
+    cartProductList: [],
+  };
+  const [cartState, dispatch] = useReducer(cartReducer, initialState);
+
+  const totalPrice = TotalCartPrice(cartState.cartProductList);
+  const totalDiscount = TotalProductDiscount(
+    cartState.cartProductList,
+    totalPrice
+  );
+  const totalAmount = (totalPrice - totalDiscount).toFixed(2);
+  const totalSave = (totalPrice - totalAmount).toFixed(2);
+  console.log(TotalProductDiscount(cartState.cartProductList, totalPrice));
+  return (
+    <cartContext.Provider
+      value={{
+        cartState,
+        dispatch,
+        totalPrice,
+        totalDiscount,
+        totalAmount,
+        totalSave,
+      }}
+    >
+      {children}
+    </cartContext.Provider>
+  );
+}
+
+const useCart = () => useContext(cartContext);
+
+export { CartProvider, useCart };
