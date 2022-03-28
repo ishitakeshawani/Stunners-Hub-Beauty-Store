e95d454b15adc198695039976f2a0e20080ef1c0
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-28 16:07:48 +0530

    added SET_CATEGORY ty

--- src/contexts/addFilters.js
+++ src/contexts/addFilters.js
@@ -2,6 +2,8 @@ export const addFilters = (state, { type, payload }) => {
   switch (type) {
     case "CALL_API":
       return { ...state, productList: payload };
+    case "SET_CATEGORY":
+      return { ...state, categoryData: payload };
     case "SORT": {
       if (payload === "PRICE_HIGH_TO_LOW") {
         console.log("1star", state);
@@ -61,7 +63,6 @@ export const addFilters = (state, { type, payload }) => {
     }
 
     case "FILTER_BY_PRICE": {
-      console.log(payload);
       return {
         ...state,
         getByPrice: payload,
