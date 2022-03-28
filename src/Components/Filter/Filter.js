beaa4952ea2fcdac2a57abbbfdfaa57689aac356
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-28 16:05:14 +0530

    did distructring of state and rename dispatch to productDispatch

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,66 +1,69 @@
 import React from "react";
-import { categories } from "../../backend/db/categories";
-import { products } from "../../backend/db/products";
 import { useProduct } from "../../contexts/ProductProvider";
 
-export const getSortedData = (state, productList) => {
-  if (state.sortBy === "PRICE_HIGH_TO_LOW") {
+export const getSortedData = ({ sortBy }, productList) => {
+  if (sortBy === "PRICE_HIGH_TO_LOW") {
     return [...productList].sort((a, b) => b.price - a.price);
   }
-  if (state.sortBy === "PRICE_LOW_TO_HIGH") {
+  if (sortBy === "PRICE_LOW_TO_HIGH") {
     return [...productList].sort((a, b) => a.price - b.price);
   }
   return productList;
 };
 
-export const filterByRate = (state, productList) => {
-  let data = [...productList];
-  if (state.filterBy === "1STAR_AND_ABOVE") {
-    data = [...productList].filter((product) => product.rate >= 1);
+export const filterByRate = ({ filterBy }, productList) => {
+  let data = productList;
+
+  if (filterBy === "1STAR_AND_ABOVE") {
+    data = productList.filter((product) => product.rate >= 1);
   }
-  if (state.filterBy === "2STAR_AND_ABOVE") {
-    data = [...productList].filter((product) => product.rate >= 2);
+  if (filterBy === "2STAR_AND_ABOVE") {
+    data = productList.filter((product) => product.rate >= 2);
   }
-  if (state.filterBy === "3STAR_AND_ABOVE") {
-    data = [...productList].filter((product) => product.rate >= 3);
+  if (filterBy === "3STAR_AND_ABOVE") {
+    data = productList.filter((product) => product.rate >= 3);
   }
-  if (state.filterBy === "4STAR_AND_ABOVE") {
-    data = [...productList].filter((product) => product.rate >= 4);
+  if (filterBy === "4STAR_AND_ABOVE") {
+    data = productList.filter((product) => product.rate >= 4);
   }
   return data;
 };
 
-export const getFilteredData = (state, productList) => {
-  let data = [...productList];
-  if (state.FilterData.filterByCategories.length !== 0) {
+export const getFilteredData = ({ FilterData }, productList) => {
+  let data = productList;
+  if (FilterData.filterByCategories.length !== 0) {
     data = data.filter((product) =>
-      state.FilterData.filterByCategories.includes(product.categoryName)
+      FilterData.filterByCategories.includes(product.categoryName)
     );
   }
 
   return data;
 };
 
-export const getFilterByPrice = (state, productList) => {
-  let data = [...productList];
-  data = data.filter((product) => product.price >= state.getByPrice);
+export const getFilterByPrice = ({ getByPrice }, productList) => {
+  let data = productList;
+  data = data.filter((product) => product.price >= getByPrice);
   return data;
 };
 
 export function Filter() {
-  const { state, dispatch } = useProduct();
+  const { state, productDispatch } = useProduct();
+  const categoriesList = state.categoryData;
 
   return (
     <div className="filter">
       <div className="filter-heading">
         <div className="semibold-font-weight">FILTERS</div>
-        <button className="clear-all-btn bold-font-weight" onClick={() =>
-          {
-            dispatch({
-              type:"CLEAR"
-            })
-          }
-        }>CLEAR</button>
+        <button
+          className="clear-all-btn bold-font-weight"
+          onClick={() => {
+            productDispatch({
+              type: "CLEAR",
+            });
+          }}
+        >
+          CLEAR
+        </button>
       </div>
       <div className="price-section">
         <div className="price semibold-font-weight price-title">Price</div>
@@ -74,7 +77,7 @@ export function Filter() {
           min="100"
           max="1000"
           onChange={(e) => {
-            dispatch({
+            productDispatch({
               type: "FILTER_BY_PRICE",
               payload: e.target.value,
             });
@@ -84,23 +87,25 @@ export function Filter() {
       </div>
       <div className="filter-category">
         <div className="semibold-font-weight category-title">Category</div>
-        {categories.map((category) => {
+        {categoriesList.map((category) => {
           return (
             <div className="inline-category-item">
-              <input
-                type="checkbox"
-                checked={state.FilterData.filterByCategories.includes(
-                  category.categoryName
-                )}
-                className="filter-input"
-                onChange={() =>
-                  dispatch({
-                    type: "FILTER_BY_CATEGORY",
-                    payload: category.categoryName,
-                  })
-                }
-              />
-              <label>{category.categoryName}</label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={state.FilterData.filterByCategories.includes(
+                    category.categoryName
+                  )}
+                  className="filter-input"
+                  onChange={() =>
+                    productDispatch({
+                      type: "FILTER_BY_CATEGORY",
+                      payload: category.categoryName,
+                    })
+                  }
+                />{" "}
+                {category.categoryName}
+              </label>
             </div>
           );
         })}
@@ -109,98 +114,116 @@ export function Filter() {
       <div className="rating-section">
         <div className="rating-title semibold-font-weight">Rating</div>
         <div className="inline-category-item">
-          <input
-            type="radio"
-            name="radio"
-            className="filter-input"
-            value="4STAR_AND_ABOVE"
-            onChange={(e) => {
-              dispatch({
-                type: "FILTER_BY_RATE",
-                payload: e.target.value,
-              });
-            }}
-          />
-          <label htmlFor="rate">4 star & above</label>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              className="filter-input"
+              checked={state.filterBy === "4STAR_AND_ABOVE"}
+              value="4STAR_AND_ABOVE"
+              onChange={(e) => {
+                productDispatch({
+                  type: "FILTER_BY_RATE",
+                  payload: e.target.value,
+                });
+              }}
+            />{" "}
+            4 star & above
+          </label>
         </div>
         <div className="inline-category-item">
-          <input
-            type="radio"
-            name="radio"
-            className="filter-input"
-            value="3STAR_AND_ABOVE"
-            onChange={(e) => {
-              dispatch({
-                type: "FILTER_BY_RATE",
-                payload: e.target.value,
-              });
-            }}
-          />
-          <label htmlFor="rate">3 star & above</label>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              className="filter-input"
+              value="3STAR_AND_ABOVE"
+              checked={state.filterBy === "3STAR_AND_ABOVE"}
+              onClick={(e) => {
+                productDispatch({
+                  type: "FILTER_BY_RATE",
+                  payload: e.target.value,
+                });
+              }}
+            />{" "}
+            3 star & above
+          </label>
         </div>
         <div className="inline-category-item">
-          <input
-            type="radio"
-            name="radio"
-            className="filter-input"
-            value="2STAR_AND_ABOVE"
-            onChange={(e) => {
-              dispatch({
-                type: "FILTER_BY_RATE",
-                payload: e.target.value,
-              });
-            }}
-          />
-          <label htmlFor="rate">2 star & above</label>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              className="filter-input"
+              value="2STAR_AND_ABOVE"
+              checked={state.filterBy === "2STAR_AND_ABOVE"}
+              onClick={(e) => {
+                productDispatch({
+                  type: "FILTER_BY_RATE",
+                  payload: e.target.value,
+                });
+              }}
+            />{" "}
+            2 star & above
+          </label>
         </div>
         <div className="inline-category-item">
-          <input
-            type="radio"
-            name="radio"
-            className="filter-input"
-            value="1STAR_AND_ABOVE"
-            onChange={(e) => {
-              dispatch({
-                type: "FILTER_BY_RATE",
-                payload: e.target.value,
-              });
-            }}
-          />
-          <label htmlFor="rate">1 star & above</label>
+          <label>
+            <input
+              type="radio"
+              name="radio"
+              className="filter-input"
+              value="1STAR_AND_ABOVE"
+              checked={state.filterBy === "1STAR_AND_ABOVE"}
+              onChange={(e) => {
+                productDispatch({
+                  type: "FILTER_BY_RATE",
+                  payload: e.target.value,
+                });
+              }}
+            />{" "}
+            1 star & above
+          </label>
         </div>
       </div>
 
       <div className="sort-by">
         <div className="sort-by-title semibold-font-weight">Sort by</div>
         <div className="inline-category-item">
-          <input
-            type="radio"
-            name="sort_by_price"
-            className="filter-input"
-            value="PRICE_HIGH_TO_LOW"
-            onChange={(e) =>
-              dispatch({
-                type: "SORT",
-                payload: e.target.value,
-              })
-            }
-          />
-          <label htmlFor="sort">Price - High to Low</label>
+          <label>
+            <input
+              type="radio"
+              className="filter-input"
+              value="PRICE_HIGH_TO_LOW"
+              name="sort"
+              checked={state.sortBy === "PRICE_HIGH_TO_LOW"}
+              onChange={(e) =>
+                productDispatch({
+                  type: "SORT",
+                  payload: e.target.value,
+                })
+              }
+            />{" "}
+            Price - High to Low
+          </label>
         </div>
         <div className="inline-category-item">
-          <input
-            type="radio"
-            name="sort_by_price"
-            className="filter-input"
-            value="PRICE_LOW_TO_HIGH"
-            onChange={(e) =>
-              dispatch({
-                type: "SORT",
-                payload: e.target.value,
-              })
-            }
-          />
-          <label htmlFor="sort">Price - Low to High</label>
+          <label>
+            <input
+              type="radio"
+              className="filter-input"
+              value="PRICE_LOW_TO_HIGH"
+              name="sort"
+              checked={state.sortBy === "PRICE_LOW_TO_HIGH"}
+              onChange={(e) =>
+                productDispatch({
+                  type: "SORT",
+                  payload: e.target.value,
+                })
+              }
+            />{" "}
+            Price - Low to High
+          </label>
         </div>
       </div>
     </div>
