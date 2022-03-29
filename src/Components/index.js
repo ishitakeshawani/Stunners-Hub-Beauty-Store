8fb5b299559ba83e95636ab4647f1bf632a3a9e8
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-29 13:01:48 +0530

    added wishlist route

--- src/Components/index.js
+++ src/Components/index.js
@@ -6,3 +6,4 @@ export { ProductsList } from "./ProductsList/ProductsList";
 export { Login } from "./Login/Login";
 export { Signup } from "./Signup/Signup";
 export { Cart } from "./Cart/Cart";
+export { Wishlist } from "./Wishlist/Wishlist";
