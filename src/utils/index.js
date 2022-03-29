28e84d2114af7c566634d45b9e9e96d45c4b5ce1
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-29 12:08:15 +0530

    moved functions to productUtils

---
+++ src/utils/index.js
@@ -0,0 +1,2 @@
+export { handleQuantity, RemoveProductFromCart, addProductToCart, TotalCartPrice, TotalProductDiscount } from "./cartUtils";
+export { getFilterByPrice, getFilteredData, filterByRate, getSortedData } from "./productUtils";
