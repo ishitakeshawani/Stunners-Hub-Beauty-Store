ccda36f2d7444c2ed587b35e74220d6f231a7ac0
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-29 13:15:29 +0530

    used useNavigate

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,7 +1,7 @@
 import { React, useState } from "react";
 import "./signup.css";
 import "../Login/login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useCart, useProduct } from "../../contexts";
 
@@ -15,6 +15,7 @@ export function Signup() {
   });
   const { setUser } = useProduct();
   const { dispatch } = useCart();
+  let navigate = useNavigate();
 
   const onHandleSubmit = async () => {
     try {
@@ -33,6 +34,7 @@ export function Signup() {
         firstName: "",
         lastName: "",
       });
+      navigate("/");
     } catch (e) {
       console.log("error", e);
     }
