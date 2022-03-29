bb37c15d963e475565311490ba93fdf260c76532
Ishita Keshawani <keshwaniishita054@gmail.com>
2022-03-29 12:07:23 +0530

    removed functions to productUtils

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,52 +1,5 @@
 import React from "react";
-import { categories } from "../../backend/db/categories";
-import { products } from "../../backend/db/products";
-import { useProduct } from "../../contexts/ProductProvider/ProductProvider";
-
-export const getSortedData = ({ sortBy }, productList) => {
-  if (sortBy === "PRICE_HIGH_TO_LOW") {
-    return [...productList].sort((a, b) => b.price - a.price);
-  }
-  if (sortBy === "PRICE_LOW_TO_HIGH") {
-    return [...productList].sort((a, b) => a.price - b.price);
-  }
-  return productList;
-};
-
-export const filterByRate = ({ filterBy }, productList) => {
-  let data = productList;
-
-  if (filterBy === "1STAR_AND_ABOVE") {
-    data = productList.filter((product) => product.rate >= 1);
-  }
-  if (filterBy === "2STAR_AND_ABOVE") {
-    data = productList.filter((product) => product.rate >= 2);
-  }
-  if (filterBy === "3STAR_AND_ABOVE") {
-    data = productList.filter((product) => product.rate >= 3);
-  }
-  if (filterBy === "4STAR_AND_ABOVE") {
-    data = productList.filter((product) => product.rate >= 4);
-  }
-  return data;
-};
-
-export const getFilteredData = ({ FilterData }, productList) => {
-  let data = productList;
-  if (FilterData.filterByCategories.length !== 0) {
-    data = data.filter((product) =>
-      FilterData.filterByCategories.includes(product.categoryName)
-    );
-  }
-
-  return data;
-};
-
-export const getFilterByPrice = ({ getByPrice }, productList) => {
-  let data = productList;
-  data = data.filter((product) => product.price >= getByPrice);
-  return data;
-};
+import { useProduct } from "../../contexts";
 
 export function Filter() {
   const { state, productDispatch } = useProduct();
